748a1831be63fbf6c7030501eaa39cbd04a5023d
barbaramariscal <barbaramariscalg94@gmail.com>
2022-02-02 20:25:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 console.log('hola')
 const volumen= 16 * 23 * 13;
-console.log('volumen', volumen + 'cm3');
+console.log('volumen', volumen + ' cm3');
 const areaBase= 16*23;
 const areaLateral=13*23; 
 const areaTotal= areaLateral + 2*areaBase;
-console.log('areaTotal', areaTotal + 'cm2');
+console.log('areaTotal', areaTotal + ' cm2');
+
+const base= 23;
+const altura= 13;
+const ancho= 16;
+const volumen2= base * altura * ancho;
+console.log('volumen2', volumen2, + ' cm3');
+const area2= base * altura + 2 * ancho * base;
+console.log('area2', area2 + ' cm2');
