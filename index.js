789de04092bbca4b5fd9f785f04b96e3ba98c39c
barbaramariscal <barbaramariscalg94@gmail.com>
2022-02-02 20:08:30 +0100

    Ejercicio1JS

---
+++ index.js
@@ -0,0 +1,7 @@
+console.log('hola')
+const volumen= 16 * 23 * 13;
+console.log('volumen', volumen + 'cm3');
+const areaBase= 16*23;
+const areaLateral=13*23; 
+const areaTotal= areaLateral + 2*areaBase;
+console.log('areaTotal', areaTotal + 'cm2');
